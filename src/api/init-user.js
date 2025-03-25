07c2016882862a028a3135f7d59af3c0709cdea7
vladislav <vlad.strebkov.00@mail.ru>
2025-03-25 09:35:00 +0300

    feat(api): добавлено получение и созранение телеграмм пользователя и пользователя

---
+++ src/api/init-user.js
@@ -0,0 +1,34 @@
+import store from "@/store";
+import { getUser } from "./user";
+
+const tgUser = {
+    name: '',
+    id: null,
+    theme: '',
+    photoUrl: ''
+  };
+
+
+  const initUser = () => {
+    const tg = window.Telegram.WebApp;
+    const initData = tg.initData;
+    if (initData) {
+      const params = new URLSearchParams(initData);
+      const userJsonString = params.get('user');
+      if (userJsonString) {
+        const userData = JSON.parse(decodeURIComponent(userJsonString));
+        tgUser.name = userData.first_name;
+        tgUser.id = userData.id;
+        tgUser.theme = tg.colorScheme;
+        tgUser.photoUrl = userData.photo_url;
+      }
+    } else {
+        tgUser.name = "Vlad";
+        tgUser.id = 2;
+        tgUser.theme = "dark";
+    }
+    store.dispatch('SET_TG_USER', tgUser);
+    store.dispatch('SET_USER', tgUser.id);
+  };
+
+  export default initUser
--- src/api/user.js
+++ src/api/user.js
@@ -1,2 +1,17 @@
 import API_BASE_URL from "@/config";
+import axios from "axios";
 
+
+const getUser = async(userId) => {
+    try{
+        const response = await axios.get(`${API_BASE_URL}/users/${userId}`);
+        return response.data
+    } catch{
+        console.error("Пользователь не найден")
+        return null
+    }
+}
+
+
+
+export {getUser}
