97b679536cd4b75ad8eb3d0cb8fa32d40f039d5c
vladislav <vlad.strebkov.00@mail.ru>
2025-03-25 09:36:29 +0300

    feat(): вызов метода инициализации пользователя

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store/index';
-
-
+import initUser from './api/init-user';
 
 const app = createApp(App);
 app.use(store);
+initUser()
 app.mount('#app');
