50d83de8e25bbd737aeb45d404db009c28f0f4c6
vladislav <vlad.strebkov.00@mail.ru>
2025-03-25 12:57:37 +0300

    feat(): список валют

---
+++ src/utils.js
@@ -0,0 +1,27 @@
+const fiatCurrencies = [
+    {name: "Рубль", code: "RUB", symbol: "₽"},
+    {name: "Доллар", code: "USD", symbol: "$"},
+    {name: "Евро", code: "EUR", symbol: "€"},
+    {name: "Иена", code: "JPY", symbol: "¥"},
+    {name: "Фунт стерлингов", code: "GBP", symbol: "£"},
+    {name: "Австралийский доллар", code: "AUD", symbol: "A$"},
+    {name: "Канадский доллар", code: "CAD", symbol: "C$"},
+    {name: "Франк", code: "CHF", symbol: "₣"},
+    {name: "Юань", code: "CNY", symbol: "¥"},
+    {name: "Шведская крона", code: "SEK", symbol: "kr"}
+]
+
+const cryptoCurrencies = [
+    {name: "Bitcoin", code: "BTC", symbol: "₿"},
+    {name: "Ethereum", code: "ЕТН"},
+    {name: "Tether USD", code: "USDT", symbol: "₮"},
+    {name: "USD Coin", code: "USDC"},
+    {name: "Ripple", code: "XRP"},
+    {name: "The Open Network", code: "TON"},
+    {name: "Solana", code: "SOL"},
+    {name: "TRON", code: "TRX"},
+    {name: "Polkadot", code: "DOT"},
+    {name: "Cardano", code: "ADA"}
+]
+
+export {fiatCurrencies, cryptoCurrencies}
