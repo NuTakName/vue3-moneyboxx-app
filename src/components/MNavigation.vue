ae7a2f2d82dc3caef892388a1aad1d7a21cc5b73
vladislav <vlad.strebkov.00@mail.ru>
2025-03-27 22:31:47 +0300

    feat(MNavigation): копонент навигации

---
+++ src/components/MNavigation.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const navigation = ["Бюджет", "Аккаунт", "Копилка"];
+const routes = ["/", "/account", "/moneybox"];
+const router = useRouter();
+</script>
+
+<template>
+  <nav>
+    <ul>
+      <li v-for="(item, index) in navigation" :key="index">
+        <router-link :to="routes[index]">{{ item }}</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+
+
+nav ul {
+  list-style-type: none;
+}
+
+nav li {
+  display: inline;
+  margin-right: 15px;
+}
+</style>
