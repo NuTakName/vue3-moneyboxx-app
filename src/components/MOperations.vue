d0522233766e929dca72c522460a61962ae01e5c
vladislav <vlad.strebkov.00@mail.ru>
2025-03-28 20:59:07 +0300

    feat(MOperations): добавлена страница операций

---
+++ src/components/MOperations.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue';
+import MMainButton from './MMainButton.vue';
+import MOperation from './MOperation.vue';
+
+let buttonName = "+ Добавить операцию"
+
+
+const isCreateOperationVisible = ref(false)
+
+const toogleIsCreateOperationVisilbe = () => {
+    isCreateOperationVisible.value = !isCreateOperationVisible.value
+}
+
+
+</script>
+
+<template>
+    <div></div>
+    <m-main-button :name="buttonName" @click="toogleIsCreateOperationVisilbe"></m-main-button>
+    <m-operation v-if="isCreateOperationVisible"></m-operation>
+</template>
+
+<style scoped>
+</style>
