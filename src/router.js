0c0f0af2d70984e32d5c595218b183cfb2987d86
vladislav <vlad.strebkov.00@mail.ru>
2025-03-28 21:00:27 +0300

    fix(src): фикс импорта компонента

--- src/router.js
+++ src/router.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
-import MOperation from "./components/MOperation.vue";
+import MOperations from "./components/MOperations.vue";
 import MAccount from "./components/MAccount.vue";
 import MMoneybox from "./components/MMoneybox.vue";
 
 
 const routes = [
-    {path: '/', name: 'Operations', component: MOperation, props: true},
+    {path: '/', name: 'Operations', component: MOperations, props: true},
     {path: '/account', name: "Account", component: MAccount, props: true},
     {path: '/moneybox', name: "Moneybox", component: MMoneybox, props: true},
 ]
