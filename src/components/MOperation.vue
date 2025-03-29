eb3bbb53c7afd956e85e47f2b10a507efbd25ad8
vladislav <vlad.strebkov.00@mail.ru>
2025-03-29 10:33:43 +0300

    fix(MOperation): добавление операции
    - закрытие окна
    - привязаны реактивные переменные

--- src/components/MOperation.vue
+++ src/components/MOperation.vue
@@ -6,6 +6,8 @@ import { expenseСategories, incomeСategories } from '@/utils';
 
 let buttonName = "Добавить"
 
+const emit = defineEmits(['close'])
+
 
 const ammount = ref()
 const description = ref()
@@ -13,10 +15,10 @@ const typeOperation = ref('income')
 const category = ref()
 const isDropDownVisible = ref(false)
 
+
 const toogleDropDownVisible = () => {
     isDropDownVisible.value = !isDropDownVisible.value
 }
-
 
 const setCategory = (data) => {
     category.value = data
@@ -31,26 +33,59 @@ const currentCategory = computed(() => {
 
 })
 
+const closeAddOperation = () => {
+    emit('close')
+}
+
+const addOperation = () => {
+    console.log("Добавили")
+    closeAddOperation()
+}
+
 
 </script>
 
 
 
 <template>
-    <div class="m-operation" @click="closeOperation">
-        <form class="m-operation-form">
+    <div class="m-operation" @click="closeAddOperation">
+        <form class="m-operation-form" @submit.prevent="addOperation">
             <label>Размер операции</label>
-            <input type="number" required v-model="ammount">
-            <label>Описание</label>
-            <input type="text" v-model="description">
+            <input 
+                type="number" 
+                class="m-operation-form-input" 
+                v-model="ammount"
+                required
+                @click.stop
+            >
+            <label class="m-operation-form-desc">Описание</label>
+            <input 
+                type="text" 
+                v-model="description"
+                class="m-operation-form-input" 
+                @click.stop
+            >
             <div class="m-operation-form-radio">
-                <input type="radio" value="income" v-model="typeOperation">Доход
-                <input type="radio" value="expense" v-model="typeOperation">Расход
+                <input 
+                    type="radio" 
+                    value="income" 
+                    v-model="typeOperation"
+                    @click.stop
+                >Доход
+                <input 
+                    type="radio" 
+                    value="expense" 
+                    v-model="typeOperation"
+                    @click.stop
+                >Расход
             </div>
             <div 
                 class="m-operation-category"
-                @click="toogleDropDownVisible"
-                >{{ currentCategory }}
+                @click.stop="toogleDropDownVisible" 
+                >Категория: {{ currentCategory }}
+            </div>
+            <div @click.stop>
+                <m-main-button :name="buttonName"></m-main-button>
             </div>
         </form>
     </div>
@@ -61,7 +96,6 @@ const currentCategory = computed(() => {
         @data="setCategory"
         >
     </m-dropdown-selector>
-    <m-main-button :name="buttonName"></m-main-button>
 </template>
 
 <style scoped>
@@ -86,24 +120,52 @@ const currentCategory = computed(() => {
     display: flex;
     align-items: center;
     flex-direction: column;
+    width: 50%;
 }
 
 .m-operation-form-radio{
     width: 100%;
     display: flex;
     justify-content: space-between;
+    margin: 10px 0px 10px 0px;
+    color: white;
+    font-size: 16px;
+    font-weight: 700;
 }
 
+
+
 .m-operation-category{
-    text-align: center;
     color: black;
-    background-color: white;
+    background-color: blue;
     padding: 10px;
     border: 3px solid black;
     border-radius: 12px;
     cursor: pointer;
-    font-size: 16px;
+    font-size: 18px;
     font-weight: 700;
+    width: 100%;
+    height: 7vh;
+    min-height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    line-height: 1;
+}
+
+label{
+    font-size: 18px;
+    color: white;
+    font-weight: 700;
+
+}
+
+.m-operation-form-desc{
+    margin-top: 10px;
+}
+
+.m-operation-form-input{
     width: 100%;
 }
 
