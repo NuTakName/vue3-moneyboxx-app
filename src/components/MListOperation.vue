7610d501069dd8f94ad41d48512e642a183c1b19
vladislav <dorchinets.ira@mail.ru>
2025-03-30 23:19:46 +0300

    fix(MListOperation): добавлены классы и стили компонента
    - добавлены методы определения класса с учетом типа операций и текущей темы
    - добавлен метод переключения окна изминения операции

--- src/components/MListOperation.vue
+++ src/components/MListOperation.vue
@@ -1,12 +1,33 @@
 <script setup>
 import { computed, ref } from 'vue';
 import { useStore } from 'vuex';
+import MOperation from './MOperation.vue';
 import { formatDate, formatTime, formatValue } from '@/utils';
+import { getOperationsByCategoryId } from '@/api/operations';
 
 
+const mark = ref()
 const store = useStore();
-const user = computed(() => store.state.user);
-const operations = computed(() => store.state.operations);
+const tgUser = computed(() => store.state.tgUser);
+const isUpdateOperationVisible = ref(false)
+const operations = ref([])
+
+const props = defineProps({
+  id: String,
+  required: true
+})
+
+const getOperations = async() => {
+  const result = await getOperationsByCategoryId(props.id)
+  operations.value = result
+}
+
+getOperations()
+
+
+const toogleUpdateOperationVisible = () => {
+  isUpdateOperationVisible.value = !isUpdateOperationVisible.value
+}
 
 
 const groupOperation = computed(() => {
@@ -20,27 +41,132 @@ const groupOperation = computed(() => {
     }, {});
 });
 
+
+const getClass = (operation) => {
+  let className
+  if (operation.type_ == "income" && tgUser.tehme == "dark") {
+    className = "dark-income"
+    mark.value = "+"
+  } else if (operation.type_ == "income" && tgUser.tehme != "dark") {
+    className = "white-income"
+    mark.value = "+"
+  } else if (operation.type_ == "expense" && tgUser.tehme == "dark") {
+    className = "dark-expense"
+    mark.value = "-"
+  } else {
+    className = "white-expense"
+    mark.value = "-"
+  }
+  return className
+};
+
+
 </script>
 
 <template>
-  <div>
-    <div> {{ operations[0].category_name }}</div>
-      <div v-for="(operations, date) in groupOperation" :key="date">
-          <div>{{ date }}</div>
-          <div v-for="(operation, index) in operations" :key="index">
-              <div>
-                  <div>
-                      <div>{{ formatValue(operation.value, operation.currency_symbol, operation.currency_code) }}</div>
-                      <div v-if="operation.description">{{ o.description }}</div>
-                  </div>
-                  <div>{{ formatTime(operation.date) }}</div>
-              </div>
-          </div>
+    <div v-if=operations.length :class="['m-list-operation-name', getClass(operations[0])]">
+       {{ operations[0].category_name }}
+    </div>
+      <div 
+        v-for="(operations, date) in groupOperation" 
+        :key="date"
+      >
+        <div :class="['m-list-operation-date', {'m-list-operation-date-dark': tgUser.theme == 'dark'} ]">
+          {{ date }}
+        </div>
+        <div v-for="(operation, index) in operations" :key="index">
+            <div 
+              :class="['m-list-operation-info-container', {'m-list-operation-info-container-dark': tgUser.theme == 'dark'}]"
+              @click="toogleUpdateOperationVisible"
+            >
+                <div class="m-list-operation-info">
+                    <div> {{ mark }} {{ formatValue(operation.value, operation.currency_symbol, operation.currency_code) }}</div>
+                    <div v-if="operation.description">{{ o.description }}</div>
+                </div>
+                <div class="m-list-operation-time">{{ formatTime(operation.date) }}</div>
+            </div>
+        </div>
       </div>
-  </div>
+      <m-operation v-if="isUpdateOperationVisible" @close="toogleUpdateOperationVisible"></m-operation>
 </template>
 
 <style scoped>
 
 
+.m-list-operation-name{
+  margin: 16px 0px 16px 0px;
+  font-size: 22px;
+  font-weight: 700;
+  width: 100%;
+  text-align: center;
+}
+
+.dark-income{
+  color: white;
+  text-shadow: 
+        -1px -1px 0 rgba(9, 112, 4, 1),  
+         1px -1px 0 rgba(9, 112, 4, 1),
+        -1px  1px 0 rgba(9, 112, 4, 1),
+         1px  1px 0 rgba(9, 112, 4, 1);
+}
+
+.dark-expense{
+  color: white;
+  text-shadow: 
+        -1px -1px 0 rgba(136, 17, 17, 1),  
+         1px -1px 0 rgba(136, 17, 17, 1),
+        -1px  1px 0 rgba(136, 17, 17, 1),
+         1px  1px 0 rgba(136, 17, 17, 1);
+}
+
+.white-income{
+  color: #097004;
+}
+
+.white-expense{
+  color: #6f0f0f;
+}
+
+.m-list-operation-date{
+  width: 100%;
+  text-align: center;
+  font-weight: 700;
+  font-size: 18px;
+  color: black;
+  margin-bottom: 16px;
+}
+
+.m-list-operation-date-dark{
+  color: white;
+  flex-grow: 1;
+  text-shadow: 
+    -1px -1px 0 black,  
+    1px -1px 0 black,
+    -1px  1px 0 black,
+    1px  1px 0 black;
+}
+
+.m-list-operation-info-container{
+  display: flex;
+  justify-content: space-between;
+  font-size: 18px;
+  color: black;
+  cursor: pointer;
+}
+
+.m-list-operation-info-container-dark{
+  color: white;
+}
+
+.m-list-operation-info{
+  width: 81px;
+  text-align: center;
+  margin-left: 20px;
+}
+
+.m-list-operation-time{
+  width: 81px;
+  text-align: center;
+  margin-right: 20px;
+}
 </style>
