99a1f4a1d177bd171b4e330cfd46aacf94175a28
vladislav <dorchinets.ira@mail.ru>
2025-04-02 13:50:10 +0300

    feat(api): добавлен метод поуления списка операций по типу и месяцу

--- src/api/operations.js
+++ src/api/operations.js
@@ -32,6 +32,16 @@ const getOperationsByCategoryId = async(categoryId, month) => {
 }
 
 
+const getOperationsByType = async (type_, month) => {
+    console.log(type_);
+    try {
+        const response = await axios.get(`${API_BASE_URL}/operations/list_by_type/${type_}/${month}`);
+        return response.data;
+    } catch (error) {
+        console.error("Не удалось получить список операций");
+    }
+}
+
 const deleteOperation = async(operationId) => {
     try{
         await axios.delete(`${API_BASE_URL}/operations/${operationId}`)
@@ -52,4 +62,4 @@ const updateOperation = async(operation) => {
 
 
 
-export {addOperation, getOperations, getOperationsByCategoryId, deleteOperation, updateOperation}
+export {addOperation, getOperations, getOperationsByCategoryId, deleteOperation, updateOperation, getOperationsByType}
