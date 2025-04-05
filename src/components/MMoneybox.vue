258c569cd3334f1e45ca7220c6bc2403f5ef2eea
vladislav <dorchinets.ira@mail.ru>
2025-04-05 13:38:19 +0300

    feat(MMoneybox): добавлена телеграмм кнопка с выходом в главное меню

--- src/components/MMoneybox.vue
+++ src/components/MMoneybox.vue
@@ -1,4 +1,15 @@
 <script setup>
+import { router } from '@/router';
+import { useStore } from 'vuex';
+
+const store = useStore();
+
+const backButton = window.Telegram.WebApp.BackButton;
+backButton.show();
+backButton.onClick(function() {
+    store.dispatch("SET_INDEX", 0)
+    router.push('/')
+})
 
 
 </script>
