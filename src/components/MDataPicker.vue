fc88d3a7c5b4c5beedb3f77a60c57fc987a741de
vladislav <dorchinets.ira@mail.ru>
2025-04-10 23:22:20 +0300

    feat(MDataPicker): добавлен датапикер дял выбора крайнего срока для копилки

---
+++ src/components/MDataPicker.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css';
+  
+const date = ref(null);
+
+</script>
+
+<template>
+    <VueDatePicker v-model="date" />
+</template>
+  
+
+
+<style>
+
+
+</style>
