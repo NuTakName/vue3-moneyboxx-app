9c4693da4277da1d6bf907aa7a6597dc9f64be7d
vladislav <dorchinets.ira@mail.ru>
2025-04-11 20:46:23 +0300

    feat(api): добавлены методы для получения списка копилок и удаления копилки

--- src/api/moneybox.js
+++ src/api/moneybox.js
@@ -51,4 +51,38 @@ const updateMoneyboxCurrentBalance = async(data) => {
     }
 
 
-export {getMoneybox, addMoneybox, updateMoneyboxCurrentBalance}
+const getMoneyboxs = async (userId) => {
+    const response = await fetch(`${API_BASE_URL}/moneybox/list/${userId}`, {
+        headers: {
+            'ngrok-skip-browser-warning': 'true',
+            'User-Agent': 'MyCustomUserAgent/1.0'
+        },
+    });
+    if (response.ok) {
+        return await response.json()
+    } else {
+        console.error("Не удалось получить список копилок")
+    }
+}
+
+const deleteMoneybox = async (moneyboxId) => {
+    const response = await fetch(`${API_BASE_URL}/moneybox/${moneyboxId}`, {
+        headers: {
+            'ngrok-skip-browser-warning': 'true',
+            'User-Agent': 'MyCustomUserAgent/1.0'
+        },
+        method: "DELETE"
+    })
+    if (!response.ok) {
+        console.error("Не удалось удалить копилку")
+    }
+    }
+
+
+export {
+    getMoneybox,
+    addMoneybox, 
+    updateMoneyboxCurrentBalance,
+    getMoneyboxs,
+    deleteMoneybox
+}
