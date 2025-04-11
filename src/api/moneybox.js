d4c4c1718b5d92cbc7b7c4522afce83c5dc44586
vladislav <dorchinets.ira@mail.ru>
2025-04-11 11:50:03 +0300

    feat(api): добавлен метод обновления баланса копилки

--- src/api/moneybox.js
+++ src/api/moneybox.js
@@ -33,5 +33,22 @@ const addMoneybox = async(moneybox) => {
     }
 
 
+const updateMoneyboxCurrentBalance = async(data) => {
+    const response = await fetch(`${API_BASE_URL}/moneybox/update_current_balance`, {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json',
+            'ngrok-skip-browser-warning': 'true',
+            'User-Agent': 'MyCustomUserAgent/1.0'
+        },
+        body: JSON.stringify(data)
+    });
+        if (response.ok) {
+        return await response.json()
+        } else {
+        console.error("Не удалось обновить баланс копилки")
+        }
+    }
 
-export {getMoneybox, addMoneybox}
+
+export {getMoneybox, addMoneybox, updateMoneyboxCurrentBalance}
