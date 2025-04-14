4e4c19dac0879a61ee6d0b6b26f39eabcf8a0538
vladislav <dorchinets.ira@mail.ru>
2025-04-14 22:14:52 +0300

    feat(api): доабвлены методя для получения статистики операций за все время, за период, получения всех операций

--- src/api/operations.js
+++ src/api/operations.js
@@ -113,6 +113,53 @@ const getDifference = async (currentBudgetId) => {
 }
 
 
+const getStatistic = async (userId) => {
+    const response = await fetch(`${API_BASE_URL}/operations/statistic/${userId}`, {
+        headers: {
+            'ngrok-skip-browser-warning': 'true',
+            'User-Agent': 'MyCustomUserAgent/1.0'
+        }
+    });
+    if (response.ok) {
+        return await response.json()
+    } else {
+        console.error("Не удалось получить данные статистики")
+    }
+}
+
+
+const getStatisticForPeriod = async(data) => {
+    const response = await fetch(`${API_BASE_URL}/operations/get_statistic_for_period`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'ngrok-skip-browser-warning': 'true',
+            'User-Agent': 'MyCustomUserAgent/1.0'
+        },
+        body: JSON.stringify(data)
+    });
+     if (response.ok) {
+        return await response.json()
+     } else {
+        console.error("Не удалось получить ститистику операций")
+     }
+    }
+
+
+const getAllOperations = async (userId) => {
+    const response = await fetch(`${API_BASE_URL}/operations/list_operations/${userId}`, {
+        headers: {
+            'ngrok-skip-browser-warning': 'true',
+            'User-Agent': 'MyCustomUserAgent/1.0'
+        }
+    });
+    if (response.ok) {
+        return await response.json()
+    } else {
+        console.error("Не удалось получить список операций")
+    }
+}
+
 export {
     addOperation,
     getOperations, 
@@ -120,5 +167,8 @@ export {
     deleteOperation, 
     updateOperation, 
     getOperationsByType,
-    getDifference
+    getDifference,
+    getStatistic,
+    getStatisticForPeriod,
+    getAllOperations
 }
